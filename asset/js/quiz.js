4427db05984f092d11252ee9e619d3994f280190
VERO0516 <veroniqueaero@gmail.com>
2022-06-26 14:52:08 +0200

    modifier quiz

--- asset/js/quiz.js
+++ asset/js/quiz.js
@@ -16,7 +16,7 @@ dateList = [{
     "title": "Quelle destination vous inspire pour votre prochain voyage ?",
     "option": ["A la Montagne", "A la Mer", "En Ville", "En Forêt"],
     "value" : [4,2,1,3],
-  },
+},
  {
   "question_number": 2,
   "title": "Quel est votre animal favori parmi la liste suivante ?",
@@ -50,45 +50,39 @@ var reponsesFinales =[
 "producteur de fromage en Auvergne"
 ];
 
-document.querySelector('#question p').innerHTML = dateList[alea[0]].title;
+
 //console.log(dateList[alea[0]].option[1].length);
 
 var h=[];
 var reponseList =[];
 
-for(var i=0;i<4; i++)
-{
-  h+=`<div><input type="radio" name="option" value="${dateList[alea[0]].value[i]}"> <label>${dateList[alea[0]].option[i]}</label></div>`;
-  //console.log(h);
-}
-document.getElementById("option").innerHTML = h;
+
+inputQuiz(0);
 
 
 function next(){
-var radio = document.getElementsByName("option");
-var check = 0;
+  var radio = document.getElementsByName("option");
+  var check = 0;
 
-for(let i=0; i<4; i++){
+  for(let i=0; i<4; i++){
 
     if(radio[i].checked){
-        reponseList.push(radio[i].value);
+        //reponseList.push(radio[i].value);
+        reponseList[j] = radio[i].value;
         check = 1;
         //console.log( reponseList);
     }
-}
-
-if(check == 1){
-  j++;
-  //console.log("j=",j);
-  document.querySelector('#question p').innerHTML = dateList[alea[j]].title;
-  let h = []; 
-  for(let i=0;i<4; i++)
-  {
-    h+=`<div><input type="radio" name="option" value="${dateList[alea[j]].value[i]}"> <label>${dateList[alea[j]].option[i]}</label></div>`;
   }
-  document.getElementById("option").innerHTML = h;
-}
-if( j == 4){
+
+  if(check == 1){
+    j++;
+    inputQuiz(j);
+    console.log("j=",j);
+    document.getElementById("last").style= "display:block";
+
+  }
+
+  if( j == 4){
     document.getElementById("finir").style= "display : block";
     document.getElementById("next").style= "display : none";
   }
@@ -96,37 +90,41 @@ if( j == 4){
 
 function finir(){
 
-var radio = document.getElementsByName("option");
-var check = 0;
-var res = 0,num=1,resN=1;
+  var radio = document.getElementsByName("option");
+  var check = 0;
+  var res = 0,num=1,resN=1;
   
-for(let i=0; i<4; i++){
-  if(radio[i].checked){
-    //console.log("yes");
-    //console.log(radio[i].value);
-    reponseList.push(radio[i].value);
-    check = 1;
-    //console.log( reponseList);
+  for(let i=0; i<4; i++){
+    if(radio[i].checked){
+      //console.log("yes");
+      //console.log(radio[i].value);
+      //reponseList.push(radio[i].value);
+      reponseList[j] = radio[i].value;
+      check = 1;
+      //console.log( reponseList);
+    }
   }
-}
 
-if(check == 1){
+  if(check == 1){
 
-  document.querySelector('#question').style= "display : none";
-  document.querySelector("#resultat").style= "display : block";
+    document.querySelector('#question').style= "display : none";
+    document.querySelector("#resultat").style= "display : block"
 
-  reponseList = reponseList.sort(
-   function(x,y){
-       if(x < y)
+    document.getElementById("finir").style= "display : none";
+    document.getElementById("next").style= "display : none";
+
+    reponseList = reponseList.sort(
+      function(x,y){
+        if(x < y)
           return -1
-      if(x >y)
-        return 1
-      return 0
-  });
-  //console.log(reponseList);
+        if(x >y)
+          return 1
+        return 0
+    });
+    //console.log(reponseList);
 
-  for(let i=0;i<4;i++){
-    //console.log("for i=",i);
+    for(let i=0;i<4;i++){
+      //console.log("for i=",i);
       if(reponseList[i] == reponseList[i+1]){
         num++;
         //console.log("i=",reponseList[i],"i+1=",reponseList[i+1]);
@@ -153,10 +151,9 @@ if(check == 1){
       else{
         num = 1;
       }
-  }
+    }
   document.querySelector('#resultat p').innerHTML = `vous êtes un ${reponsesFinales[res-1]}`;
-
-}
+  }
 
 }
 
@@ -167,23 +164,50 @@ function refaire(){
 
     document.getElementById("finir").style= "display : none";
     document.getElementById("next").style= "display : block";
+    document.getElementById("last").style= "display : none";
     j = 0;
     alea = [0,1,2,3,4];
     alea = alea.sort(function(){
         return .5 - Math.random();
     })
-
-    document.querySelector('#question p').innerHTML = dateList[alea[0]].title;
-    //console.log(dateList[alea[0]].option[1].length);
-
-    h=[];
     reponseList =[];
+    inputQuiz(0);
 
-    for(var i=0;i<4; i++)
-    {
-        h+=`<div><input type="radio" name="option" value="${dateList[alea[0]].value[i]}"> <label>${dateList[alea[0]].option[i]}</label></div>`;
-        //console.log(h);
+}
+
+function last(){
+  if(j == 1){
+    document.getElementById("last").style= "display : none";
+  }
+  j--;
+  inputQuiz(j)
+}
+
+
+function inputQuiz(j){
+
+  document.querySelector('#question p').innerHTML = dateList[alea[j]].title;
+  let h = []; 
+  for(let i=0;i<4; i++)
+  {
+    console.log("123=",reponseList[j]);
+
+    if(dateList[alea[j]].value[i] == reponseList[j]){
+      h+=`<div>
+      <input type="radio" name="option" id="radio" checked value="${dateList[alea[j]].value[i]}"> 
+      <label for="radio"></label>
+      <span>${dateList[alea[j]].option[i]}</span>
+      </div>`;
     }
-    document.getElementById("option").innerHTML = h;
+    else{
+      h+=`<div>
+      <input type="radio" name="option" id="radio" value="${dateList[alea[j]].value[i]}"> 
+      <label for="radio"></label>
+      <span>${dateList[alea[j]].option[i]}</span>
+      </div>`;
+    }
+  }
+  document.getElementById("option").innerHTML = h;
+
 
 }
