7697b2ad26acf563186a92255e5e42b8885d2326
Mistrale94 <quat94@gmail.com>
2022-06-28 23:06:06 +0200

    test cookies

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,9 +1,9 @@
 
 const btnToggle = document.querySelector('.btn-toggle');
 
-btnToggle.addEventListener('click touchstart', () => {
+btnToggle.addEventListener('click', () => {
     const body = document.body;
-    var image = document.getElementById('darkmode darkmode-mobile');
+    var image = document.getElementById('darkmode');
     var logo = document.getElementById('logo');
     var burger = document.getElementById('img-burger');
     var picto = document.getElementById('picto');
@@ -40,3 +40,10 @@ function menu() {
         document.getElementById('burger').classList.add('open');
     }
 }
+
+void 0 === window._axcb && (window._axcb = []);
+window._axcb.push(function(axeptio) {
+  axeptio.on("cookies:complete", function(choices) {
+    
+  })
+})
