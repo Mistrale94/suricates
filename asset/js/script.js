c8a1bfe5458a8392c569a6ff4d4008de20cbde41
VERO0516 <veroniqueaero@gmail.com>
2022-06-30 11:20:21 +0200

    modifier css

--- asset/js/script.js
+++ asset/js/script.js
@@ -8,25 +8,32 @@ function btntoggle(){
     var picto = document.getElementById('picto');
     var picto2 = document.getElementById('picto2');
     var picto3 = document.getElementById('picto3');
+    var image_mobile = document.querySelector('.mobile-nav .mobile_link-group .btn-toggle img');
 
     if(body.classList.contains('dark')){
         body.classList.add('light')
         body.classList.remove('dark')
         image.src = "asset/image/akar-icons_moon-fill.png";
+        image_mobile.src = "asset/image/akar-icons_moon-fill.png";
+
         logo.src = "asset/image/logo.png";
         burger.src = "asset/image/Menu.png";
         picto.src = "asset/image/Illu Valeur.png";
         picto2.src = "asset/image/Illu Valeur (1).png";
         picto3.src = "asset/image/Illu Valeur (2).png";
+
     }else if (body.classList.contains('light')){
         body.classList.add('dark')
         body.classList.remove('light')
         image.src = "asset/image/akar-icons_sun-fill.png";
+        image_mobile.src = "asset/image/akar-icons_sun-fill.png";
+
         logo.src = "asset/image/logo-white.png";
         burger.src = "asset/image/Menu (1).png";
         picto.src = "asset/image/Illu Valeur (3).png";
         picto2.src = "asset/image/Illu Valeur (4).png";
         picto3.src = "asset/image/Illu Valeur (5).png";
+
     }
 
 }
